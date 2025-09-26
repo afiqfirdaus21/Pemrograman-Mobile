ef9c8ebf870e85c3c4a9549c8ee31dc2f883d52d
Afiq Firdaus <fafiq8445@gmail.com>
2025-09-26 10:37:31 +0700

    tugas praktikum

---
+++ Pertemuan 4/tugas praktikum/bin/tugas_praktikum.dart
@@ -0,0 +1,7 @@
+// import 'package:tugas_praktikum/tugas_praktikum.dart' as tugas_praktikum;
+
+void main(List<String> arguments) {
+  print('halo, ini projek tugas pertemuan 4');
+
+  // tugas_praktikum.main(arguments);
+}
