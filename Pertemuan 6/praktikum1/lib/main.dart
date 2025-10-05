cc5ce64227f82c7da8049e84a4acd5289451d898
Afiq Firdaus <fafiq8445@gmail.com>
2025-10-05 12:42:41 +0700

    drawer

--- Pertemuan 6/praktikum1/lib/main.dart
+++ Pertemuan 6/praktikum1/lib/main.dart
@@ -10,22 +10,53 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Belajar Scaffold & AppBar"),
-          backgroundColor: Colors.teal,
-          actions: const [
-            Icon(Icons.search),
-            Icon(Icons.settings),
+      title: 'Contoh Drawer',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Contoh Drawer"),
+        backgroundColor: Colors.teal,
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
           ],
         ),
-        body: const Center(
-          child: Text("Ini Body Aplikasi"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add),
-        ),
+      ),
+      body: const Center(
+        child: Text("Halo Flutter dengan Drawer!"),
       ),
     );
   }
